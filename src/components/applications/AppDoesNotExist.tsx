24d488c6dba2ba5123773841bad4ae1b2141c767
Furkan Abay <furkan_abay@hotmail.com>
2025-03-07 05:06:15 +0100

    feat: fixed large scale on the AppDoesNotExist component

--- src/components/applications/AppDoesNotExist.tsx
+++ src/components/applications/AppDoesNotExist.tsx
@@ -16,18 +16,18 @@ export default function AppDoesNotExist({ application_name }: { application_name
         top: `${position.y}px`,
         left: `${position.x}px`
       }}
-      className="bg-[#E4E4E4] w-[600px] h-[222px] rounded-[13px] overflow-hidden flex flex-col absolute top-24">
-      <div onMouseDown={handleMouseClick} className="bg-gradient-to-b from-[#DEDDDE] to-[#CBC9CC] h-[14%] border-b-2 border-[#A8A8A8]" />
-      <div className="flex px-8 mt-8">
+      className="bg-[#E4E4E4] w-[450px] h-[166px] rounded-[13px] overflow-hidden flex flex-col absolute top-24 shadow-2xl">
+      <div onMouseDown={handleMouseClick} className="bg-gradient-to-b from-[#DEDDDE] to-[#CBC9CC] h-[15%] border-b-2 border-[#A8A8A8]" />
+      <div className="flex px-5 pt-5 pb-3">
         <Alert />
         <div className="ml-10 h-full">
-          <h5 className="font-black text-lg">"{appNameCapital}" is still under development.</h5>
-          <p>This app needs to be updated by its developer to improve compatibility.</p>
+          <h5 className="font-black text-base">"{appNameCapital}" is still under development.</h5>
+          <p className="text-sm">This app needs to be updated by its developer to improve compatibility.</p>
         </div>
       </div>
       <button
         onClick={() => closeApp(application_name)}
-        className="bg-gradient-to-b from-[#6BA6EC] to-[#2F7CEE] border border-[#3D80EB] w-max px-10 text-white rounded-[7px] ml-auto mr-8">OK</button>
+        className="bg-gradient-to-b from-[#6BA6EC] to-[#2F7CEE] border border-[#3D80EB] w-max px-8 text-white rounded-[7px] ml-auto mr-8">OK</button>
     </div>
   )
 }
--- src/components/svgs/Alert.tsx
+++ src/components/svgs/Alert.tsx
@@ -1,21 +1,23 @@
 export default function Alert() {
   return (
-    <svg width="114" height="102" viewBox="0 0 114 102" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="114" height="60" viewBox="0 0 114 101" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clipPath="url(#clip0_506_21)">
-        <path d="M67.3923 8L109.828 81.5C114.446 89.5 108.673 99.5 99.4352 99.5H14.5648C5.32717 99.5 -0.446346 89.5 4.17245 81.5L46.6077 8.00001C51.2265 5.72205e-06 62.7735 9.53674e-07 67.3923 8Z" fill="url(#paint0_linear_506_21)" stroke="white" strokeWidth="4" />
-        <path d="M56.9365 67.7679C58.4622 66.4329 61.7043 43.0548 63.1346 31.5326C63.6909 28.9103 63.1347 23.189 56.9365 23.189C50.6315 23.1889 50.1821 28.5925 50.7384 31.5326C52.1687 44.1673 55.4108 69.1029 56.9365 67.7679Z" fill="white" />
-        <circle cx="56.7616" cy="82.7865" r="7.1517" fill="white" />
+        <path d="M67.5677 8.35108L108.985 80.0879C113.604 88.0879 107.83 98.0879 98.5927 98.0879H15.7582C6.52056 98.0879 0.747057 88.0879 5.36586 80.0879L46.7831 8.35107C51.4019 0.35107 62.9489 0.351074 67.5677 8.35108Z" fill="url(#paint0_linear_506_21)" stroke="white" strokeWidth="4" />
+        <path d="M57.1129 66.8706C58.6153 65.556 61.8079 42.5347 63.2164 31.1885C63.7642 28.6062 63.2164 22.9722 57.1129 22.9722C50.9041 22.9721 50.4616 28.2932 51.0094 31.1885C52.4179 43.6303 55.6105 68.1852 57.1129 66.8706Z" fill="white" />
+        <circle cx="56.9407" cy="81.6602" r="7.04253" fill="white" />
       </g>
       <defs>
-        <linearGradient id="paint0_linear_506_21" x1="56.7616" y1="-14" x2="57" y2="140" gradientUnits="userSpaceOnUse">
+        <linearGradient id="paint0_linear_506_21" x1="56.9407" y1="-13.6489" x2="57.1754" y2="138" gradientUnits="userSpaceOnUse">
           <stop stopColor="#F4DC62" />
           <stop offset="0.5" stopColor="#DDB941" />
           <stop offset="1" stopColor="#D3A536" />
         </linearGradient>
         <clipPath id="clip0_506_21">
-          <rect width="114" height="102" fill="white" />
+          <rect width="114" height="101" fill="white" />
         </clipPath>
       </defs>
     </svg>
+
+
   )
 }
